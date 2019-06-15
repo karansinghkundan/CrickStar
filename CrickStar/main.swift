01751226ddb9c7d0d9ad368c58952e38950de9f1
Tarlochan <brotherstarlochan5268@gmail.com>
2019-06-15 18:39:49 -0400

    Initial Commit

---
+++ CrickStar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CrickStar
+//
+//  Created by admin on 2019-06-15.
+//  Copyright © 2019 CrickStar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
